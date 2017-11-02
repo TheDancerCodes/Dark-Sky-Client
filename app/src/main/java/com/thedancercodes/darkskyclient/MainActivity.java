f4e5f9a95e57882df57913d4ca710f79d85f1e49
Roger Taracha <rojtaracha@gmail.com>
2017-11-02 14:26:11 +0300

    [FEATURE] Get API Response using Retrofit PT1: [+] Add Retrofit dependency. [+] Add Initial creation object of Retrofit in MainActivity.

--- app/src/main/java/com/thedancercodes/darkskyclient/MainActivity.java
+++ app/src/main/java/com/thedancercodes/darkskyclient/MainActivity.java
@@ -3,11 +3,18 @@ package com.thedancercodes.darkskyclient;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import retrofit2.Retrofit;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initial creation object of Retrofit
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.darksky.net/forecast/290132a90079d32d3f55f4a0e4364149/1.2921,36.8219")
+                .build();
     }
 }
