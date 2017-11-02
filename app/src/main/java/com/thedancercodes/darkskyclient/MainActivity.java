82b0ee1e6a364d0e504e91f34dbbf6c862de4f4e
Roger Taracha <rojtaracha@gmail.com>
2017-11-02 20:58:15 +0300

    [FEATURE] Update UI Using Butterknife: [+] Import the Butterknife dependency. [+] Annotate fields with @BindView and a view ID for Butter Knife to find and automatically cast the corresponding view in your layout.

--- app/src/main/java/com/thedancercodes/darkskyclient/MainActivity.java
+++ app/src/main/java/com/thedancercodes/darkskyclient/MainActivity.java
@@ -3,11 +3,14 @@ package com.thedancercodes.darkskyclient;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.thedancercodes.darkskyclient.services.WeatherService;
 import com.thedancercodes.darkskyclient.services.WeatherServiceProvider;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import models.Currently;
 import models.Weather;
 import retrofit2.Call;
@@ -20,12 +23,19 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    @BindView(R.id.tempTextView)
+    TextView tempTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         requestCurrentWeather(1.2921, 36.8219);
+
+        // Referencing textView
+        //tempTextView = (TextView) findViewById(R.id.tempTextView);
+        ButterKnife.bind(this);
     }
 
     private void requestCurrentWeather(double lat, double lng) {
