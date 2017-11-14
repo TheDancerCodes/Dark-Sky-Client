886b1d6bc6191fd37d930840544550a45529c5d8
Roger Taracha <rojtaracha@gmail.com>
2017-11-14 11:48:51 +0300

    [FEATURE] Create UI Part1: [+] Add Drawable assets for the different screen densities. [+] Update icon and summary text from MainActivity.     [=] Create a HashMap and assign icon drawables for icon names.

--- app/src/main/java/com/thedancercodes/darkskyclient/MainActivity.java
+++ app/src/main/java/com/thedancercodes/darkskyclient/MainActivity.java
@@ -3,6 +3,7 @@ package com.thedancercodes.darkskyclient;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,6 +15,9 @@ import com.thedancercodes.darkskyclient.services.WeatherServiceProvider;
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -31,6 +35,12 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.tempTextView)
     TextView tempTextView;
+
+    @BindView(R.id.summaryTextView)
+    TextView summaryTextView;
+
+    @BindView(R.id.iconImageView)
+    ImageView iconImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,16 +67,37 @@ public class MainActivity extends AppCompatActivity {
         EventBus.getDefault().unregister(this);
     }
 
-    // Subscrice to receive the Weather event
+    // Subscribe to receive the Weather event
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onWeatherEvent(WeatherEvent weatherEvent) {
 
         // When weather event is triggered, you get the weather here
         Currently currently = weatherEvent.getWeather().getCurrently();
         tempTextView.setText(String.valueOf(Math.round(currently.getTemperature())));
+
+        // Setting the summary text
+        summaryTextView.setText(currently.getSummary());
+
+        // Create a HashMap and assign icon drawable for icon name.
+        Map<String, Integer> iconMap = new HashMap<>();
+        iconMap.put("clear-day", R.drawable.ic_clear_day);
+        iconMap.put("clear-night", R.drawable.ic_clear_night);
+        iconMap.put("rain", R.drawable.ic_rain);
+        iconMap.put("snow", R.drawable.ic_snow);
+        iconMap.put("sleet", R.drawable.ic_sleet);
+        iconMap.put("wind", R.drawable.ic_wind);
+        iconMap.put("fog", R.drawable.ic_fog);
+        iconMap.put("cloudy", R.drawable.ic_cloudy);
+        iconMap.put("partly-cloudy-day", R.drawable.ic_partly_cloudy_day);
+        iconMap.put("partly-cloudy-night", R.drawable.ic_partly_cloudy_night);
+        iconMap.put("thunderstorm", R.drawable.ic_thunderstorm);
+
+        // Setting the icon dynamically
+        iconImageView.setImageResource(iconMap.get(currently.getIcon()));
+
     }
 
-    // Subscrice to receive the Error event
+    // Subscribe to receive the Error event
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onErrorEvent(ErrorEvent errorEvent) {
         Toast.makeText(this, errorEvent.getErrorMessage(), Toast.LENGTH_SHORT).show();
